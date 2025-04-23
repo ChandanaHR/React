2fc3582bcd8b7091d29649d36ae829b5bacadf2d
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2025-04-23 11:46:56 +0530

    Create Handlinginputfieldsstate.jsx

---
+++ Handlinginputfieldsstate.jsx
@@ -0,0 +1,68 @@
+//Handling input fields in react
+//state.jsx
+//First way
+import {useState} from 'react'
+function Input() {
+    //const[name,setname] = useState("");
+    function handlechange(e) {
+        console.log("Change occurred")
+    }
+    return (
+        <div>
+            <form>
+               <h3>Enter your name: </h3> <input onChange={handlechange} type="text" value={name}></input>
+            </form>
+        </div>
+    )
+}
+export default Input
+
+//second way
+import {useState} from 'react'
+function Input() {
+    const[name,setname] = useState("");
+    function handlechange(e) {
+        console.log(e.target.value)
+    }
+    return (
+        <div>
+            <form>
+               <h3>Enter your name: </h3> <input onChange={function demo(e){
+                                                            return handlechange(e)
+                                                          }} type="text" value={name}></input>
+            </form>
+        </div>
+    )
+}
+export default Input
+
+//Third way
+import {useState} from 'react'
+function Input() {
+    const[name,setname] = useState("");
+    function handlechange(e) {
+        setname(e.target.value)
+    }
+    return (
+        <div>
+            <form>
+               <h3>Enter your name: </h3> <input onChange={function demo(e){
+                                                            return handlechange(e)
+                                                          }} type="text" value={name}></input>
+            </form>
+        </div>
+    )
+}
+export default Input
+
+//App.jsx
+import Input from "./components/Input"
+function App() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
+export default App
+
