5a268006a8c66edd1261e8de6d37bd331249333d
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-03-05 16:49:31 +0530

    Update Fetchapi.jsx

--- Fetchapi.jsx
+++ Fetchapi.jsx
@@ -53,3 +53,69 @@ createRoot(document.getElementById('root')).render(
     </div>
   </StrictMode>,
 )
+
+
+
+
+import {useState,useEffect} from "react"
+export function Useeffect() {
+    const[count,setcount] = useState(0)
+    const[count1,setcount1] = useState(0)
+    useEffect(()=>{
+        document.title = `Count: ${count}`;
+    },[])
+    return (
+        <div>
+            <h1>Count:{count}</h1>
+            <button onClick={()=>setcount(count+1)}>Increment</button>
+            <h1>Count1:{count1}</h1>
+            <button onClick={()=>setcount1(count1+1)}>Increment1</button>
+        </div>
+    )
+}
+
+// Basic syntax: fetch("url")
+//               .then(response => response.json())
+//               .then(data=>{console.log(data)})
+export function Fetchapi() {
+    const[users,setusers] = useState([])
+    useEffect(()=>{
+        fetch("https://emojihub.yurace.pro/api/all")
+        .then(res => res.json())
+        .then(data => setusers(data))
+    },[])
+    return (
+        <div>
+            <ul>
+                {users.map(user=>{
+                    return <li>{user.name}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export function Async() {
+    const[users1,setusers1] = useState([]);
+    useEffect(()=>{
+        async function fetchusers() {
+            try {
+                const response = await fetch("https://dog.ceo/api/breed/hound/images");
+                const data = await response.json();
+                setusers1(data.message)
+                console.log(data)
+            }
+            catch(error) {
+                console.log("Error fetch",error);
+            }
+        }
+        fetchusers()
+    },[])
+    return (
+        <div>
+           {users1.map((img,index)=>(
+            <img src={img} width="200"></img>
+           ))}
+        </div>
+    )
+}
